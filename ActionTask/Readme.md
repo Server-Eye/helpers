52212b11c1e77f67e2dd39117d12557576353dbe
Andreas Behr <andreas.behr@kraemer-it.de>
2018-01-22 14:52:47 +0100

    refactored to better reflect the rest of the helper project.

--- ActionTask/Readme.md
+++ ActionTask/Readme.md
@@ -1,17 +1,17 @@
 # Action Task
 
-Executes an user defined action based on the state of a given agent.
+Executes an user defined action based on the state of a given sensor.
 
-This script should be used as an automated windows task. It polls the state of a given agent and executes a custom action based on the state of the agent or other criteria.
+This script should be used as an automated windows task. It polls the state of a given sensor and executes a custom action based on the state of the sensor or other criteria.
 
 ## Configure script
 The following parameters can be passed via command line or hardcoded into the script. They are both mandatory.
 
-### apiKey 
+### AuthToken 
 The api key required for the request. Can be created via the ServerEye Powershell helpers and the function New-SeApiApiKey
 
-### agentID
-The agent ID represents the agent of which the state will be monitored. You can find the ID in the settings panel of an agent in your OCC.
+### SensorId
+The sensor ID represents the sensor of which the state will be monitored. You can find the ID in the settings panel of a sensor in your OCC.
 
 ## Running the script as task
 Follow [these instructions](https://social.technet.microsoft.com/wiki/contents/articles/38580.configure-to-run-a-powershell-script-into-task-scheduler.aspx) to learn how to configure a scheduled windows task and how to execute a powershell script as windows task.
--- ActionTask/ServerEye.PowerShell.ActionTask.ps1
+++ ActionTask/ServerEye.PowerShell.ActionTask.ps1
@@ -1,96 +1,93 @@
 ﻿ <# 
-.SYNOPSIS
-Executes an user defined action based on the condition of a given agent.
-.DESCRIPTION
-Creates a new session for interacting with the Server-Eye cloud. Then the state of an given agent will be received
-and if the agent is in an error state a defined action will be executed.
-.PARAMETER apiKey 
-The neccessary api key required for the request. Can be created via the ServerEye Powershell helpers and the function New-SeApiApiKey
-.PARAMETER agentID
-This neccessary agent ID, representing the agent you want to be monitored.You can find the ID in the settings panel of an agent (OCC)
+    .SYNOPSIS
+    Executes an user defined action based on the condition of a given agent.
+
+    .DESCRIPTION
+    Creates a new session for interacting with the Server-Eye cloud. Then the state of an given agent will be received
+    and if the agent is in an error state a defined action will be executed.
+
+    .PARAMETER AuthToken 
+    The neccessary api key required for the request. Can be created via the ServerEye Powershell helpers and the function New-SeApiApiKey
+    
+    .PARAMETER SensorId
+    This neccessary agent ID, representing the agent you want to be monitored.You can find the ID in the settings panel of an agent (OCC)
 #>
  
- Param(
-        [Parameter(Mandatory=$true)] 
-        $apiKey = "",
+Param(
+    [Parameter(Mandatory=$true)] 
+    $AuthToken,
 
-        [Parameter(Mandatory=$true)] 
-        $agentID = ""
+    [Parameter(Mandatory=$true)] 
+    $SensorId
  )
 
- $exitCode = -1
- $requirementsFullfilled = $false
-
- if([System.String]::IsNullOrEmpty($apiKey) -or ([System.String]::IsNullOrEmpty($agentID))){
-
-    $exitCode = -2
-    Write-Host "Please enter all required parameters (apiKey,agentID)"
-
- }else{
+Begin{
+         
+    $exitCode = -1
+    $requirementsFullfilled = $false
 
     if( Get-Module -ListAvailable -Name ServerEye.PowerShell.Helper) {
-
+     
         if( !(Get-Module -Name ServerEye.PowerShell.Helper) ){
             Import-Module -Name ServerEye.Powershell.Helper
         }
-
+    
         Write-Host "Helpers found..Continue with script"
         $requirementsFullfilled=$true
-
+    
     }else{
-
+    
         try{
-
+    
             Install-Module -Name ServerEye.Powershell.Helper -Scope CurrentUser -SkipPublisherCheck $true
             Write-Host "Helpers not found..installed module!"
             $requirementsFullfilled=$true
-
+    
         }catch{
             $exitCode = -3
-            Write-Host "Helpers not found and could not be installed. Please refer to https://github.com/Server-Eye/helpers/tree/master/ServerEye.Powershell.Helper for more information"
+            Write-Error -Message "Helpers not found and could not be installed. Please refer to https://github.com/Server-Eye/helpers/tree/master/ServerEye.Powershell.Helper for more information" -ErrorId 3
+            exit $exitCode
         }
     }
+
+    $AuthToken = Test-Auth -AuthToken $AuthToken
+}
+
+Process {
 
     if($requirementsFullfilled){
-
         try{
-
             $sensorData = Get-SESensorState -AuthToken $apiKey -SensorId $agentID
-
+     
             if($sensorData.Error){
-
                 Write-Host "Sensor state is ERROR..executing user defined option"
-                <# specify option you want to do! 
-                    Examples are 
-                    Stop-Service "Servicename"
-                    Restart-Service "Servicename"
-                    Start-Service "Servicename"
-
-                    Stop-Process "processname"
-                    Start-Process -FilePath "test.exe" -WorkingDirectory -C:\temp"
-
-                    Restart-Computer
-
-                    Anything you want to do
-                #>
-
+                 <# specify option you want to do! 
+                     Examples are 
+                     Stop-Service "Servicename"
+                     Restart-Service "Servicename"
+                     Start-Service "Servicename"
+     
+                     Stop-Process "processname"
+                     Start-Process -FilePath "test.exe" -WorkingDirectory -C:\temp"
+     
+                     Restart-Computer
+     
+                     Anything you want to do
+                 #>
+     
                 Restart-Service wmiApSrv
-
-            }else{
-
+                $exitCode = 0
+    
+            }else{  
                 $exitCode = 0
                 Write-Host "Sensor state is OK"
-
             }
-
-        }catch{
-
-             Write-Host "Could not receive sensor state: $_."
-             $exitCode = -4
-             
-        }
-    }
-
+     
+        }catch{ 
+            Write-Error "Could not receive sensor state: $_."
+            $exitCode = -4             
+         }
+     }
+     
+    exit $exitCode
  }
-
- exit $exitCode
