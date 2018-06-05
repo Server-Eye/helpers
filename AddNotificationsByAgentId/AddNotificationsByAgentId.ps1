c3f782d74f6e8525062495b32285dabad5318581
Rene Thulke <rene.thulke@server-eye.de>
2018-06-05 11:00:37 +0200

    Changed to new Output

--- AddNotificationsByAgentId/AddNotificationsByAgentId.ps1
+++ AddNotificationsByAgentId/AddNotificationsByAgentId.ps1
@@ -1,20 +1,19 @@
 [CmdletBinding()]
 Param(
-    [Parameter(ValueFromPipeline=$true)]
-    [alias("ApiKey","Session")]
+    [Parameter(ValueFromPipeline = $true)]
+    [alias("ApiKey", "Session")]
     $AuthToken,
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory = $True)]
     [string]$userID,
-    [Parameter(Mandatory=$True)]
-    [string]$SensorType,
+    [Parameter(Mandatory = $True)]
+    [string]$AgentType,
     [switch]$email,
     [switch]$Phone,
     [switch]$ticket,
-    [string]$deferId=""
+    [string]$deferId = ""
 )
 
 $AuthToken = Test-SEAuth -AuthToken $AuthToken
-$result = @()
 
 $customers = Get-SeApiMyNodesList -Filter customer -AuthToken $AuthToken
 
@@ -30,90 +29,99 @@ foreach ($customer in $customers) {
 
                     $agents = Get-SeApiContainerAgentList -AuthToken $AuthToken -CId $sensorhub.id
 
-                     foreach ($agent in $agents) {
+                    foreach ($agent in $agents) {
                                                                                             
-                        if ($agent.subtype -eq $SensorType) {
+                        if ($agent.subtype -eq $AgentType) {
 
-                        $notifications = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $agent.id
+                            $notifications = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $agent.id
 
-                        if ($notifications){
+                            if ($notifications) {
 
-                          foreach ($notification in $notifications) {
+                                foreach ($notification in $notifications) {
 
-                            if ($notification.userId -eq $userID) {
-                                $currentnotifcations = Set-SeApiAgentNotification -AuthToken $AuthToken -AId $agent.id -NId $notification.nId -Email $email.IsPresent -Phone $Phone.IsPresent -Ticket $ticket.IsPresent
+                                    if ($notification.userId -eq $userID) {
+                                        $currentnotifcations = Set-SeApiAgentNotification -AuthToken $AuthToken -AId $agent.id -NId $notification.nId -Email $email.IsPresent -Phone $Phone.IsPresent -Ticket $ticket.IsPresent
 
-                            if ($currentnotifcations) {
+                                        if ($currentnotifcations) {
 
-                               foreach ($currentnotifcation in $currentnotifcations) {
-                                $currentnotifcation = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $currentnotifcation.aId | Where-Object {$_.Nid -eq $currentnotifcation.nId}     
-                                $out = New-Object psobject
-                                $out | Add-Member NoteProperty Kunde ($customer.name)
-                                $out | Add-Member NoteProperty Netzwerk ($container.name)
-                                $out | Add-Member NoteProperty Server ($sensorhub.name)
-                                $out | Add-Member NoteProperty Sensor ($agent.name)
-                                if ($currentnotifcation.isGroup -eq $true) {
-                                    $out | Add-Member NoteProperty Benachrichtigung ($currentnotifcation.surname)
-                                } 
-                                else {
-                                    $out | Add-Member NoteProperty Benachrichtigung ($currentnotifcation.useremail)
-                                }
-                                $out | Add-Member NoteProperty EMail ($currentnotifcation.email)
-                                $out | Add-Member NoteProperty SMS ($currentnotifcation.phone)
-                                $out | Add-Member NoteProperty Tanss ($currentnotifcation.ticket)
-                                if ($deferId -ne ""){
-                                $snn = Set-SeApiAgentNotification -AuthToken $AuthToken -AId $agent.id -NId $nnotification.nId -DeferId $deferId 
-                                $gnn = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $Snn.aId | Where-Object {$_.Nid -eq $nnotification.nId}
-                                $out | Add-Member NoteProperty Verzoegertszeit ($gnn.deferTime)
-                                $out | Add-Member NoteProperty Verzoegertsname ($gnn.deferName)
-                                }
-                                $out | Add-Member NoteProperty Zustand ("Schon vorhanden, gegebenfalls ver�ndert!")
-                                $result += $out 
-                                }
-                            }
+                                            foreach ($currentnotifcation in $currentnotifcations) {
+                                                $currentnotifcation = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $currentnotifcation.aId | Where-Object {$_.Nid -eq $currentnotifcation.nId} 
+                                                
+                                                [PSCustomObject]@{
+                                                    Customer     = $customer.name
+                                                    Network      = $container.name
+                                                    Server       = $sensorhub.name
+                                                    Agent        = $agent.name
+                                                    Notification = if ($currentnotifcation.isGroup -eq $true) {
+                                                        $currentnotifcation.surname
+                                                    }
+                                                    else {
+                                                        "$($currentnotifcation.prename) $($currentnotifcation.surname)".Trim() 
+                                                    }
+                                                    EMail        = $currentnotifcation.email
+                                                    Phone        = $currentnotifcation.phone
+                                                    Ticket       = $currentnotifcation.ticket
+                                                    Defer        = if ($deferId -ne "") {
+                                                        $snn = Set-SeApiAgentNotification -AuthToken $AuthToken -AId $agent.id -NId $nnotification.nId -DeferId $deferId 
+                                                        $gnn = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $Snn.aId | Where-Object {$_.Nid -eq $nnotification.nId}
+                                                        [PSCustomObject]@{
+                                                            Defertime = $gnn.deferTime
+                                                            Defername = $gnn.deferName
+                                                        }   
+                                                    }
+                                                    else {
+                                                        "No Deferid was set."
+                                                    }
+                                                    State        = "Notification was present, maybe changed."
+                                                }
+                                            }
+                                        }
                                                      
-                         }
-                         }
-                         }
-                        else {
-
-                        $nnotifications = New-SeApiAgentNotification -AuthToken $AuthToken -AId $agent.id -UserId $userID -Email $email.IsPresent -Phone $Phone.IsPresent -Ticket $ticket.IsPresent
-
-                        if ($nnotifications) {
-                               foreach ($nnotification in $nnotifications) {
-                               $nnotification = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $nnotification.aId | Where-Object {$_.Nid -eq $nnotification.nId}
-
-                                $out = New-Object psobject
-                                $out | Add-Member NoteProperty Kunde ($customer.name)
-                                $out | Add-Member NoteProperty Netzwerk ($container.name)
-                                $out | Add-Member NoteProperty Server ($sensorhub.name)
-                                $out | Add-Member NoteProperty Sensor ($agent.name)
-                                if ($nnotification.isGroup -eq $true) {
-                                    $out | Add-Member NoteProperty Benachrichtigung ($nnotification.surname)
-                                } 
-                                else {
-                                    $out | Add-Member NoteProperty Benachrichtigung ($nnotification.useremail)
+                                    }
                                 }
-                                $out | Add-Member NoteProperty EMail ($nnotification.email)
-                                $out | Add-Member NoteProperty SMS ($nnotification.phone)
-                                $out | Add-Member NoteProperty Tanss ($nnotification.ticket)
-                                if ($deferId -ne ""){
-                                $snn = Set-SeApiAgentNotification -AuthToken $AuthToken -AId $agent.id -NId $nnotification.nId -DeferId $deferId 
-                                $gnn = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $Snn.aId | Where-Object {$_.Nid -eq $nnotification.nId}
-                                $out | Add-Member NoteProperty Verzoegertszeit ($gnn.deferTime)
-                                $out | Add-Member NoteProperty Verzoegertsname ($gnn.deferName)
-                                }
-                                $out | Add-Member NoteProperty Zustand ("Neu")
-                                $result += $out
-                                }           
                             }
-                            }                         
-}                                                         
-}                 
-}
-}
-}
-}
-}
-$result
+                            else {
 
+                                $nnotifications = New-SeApiAgentNotification -AuthToken $AuthToken -AId $agent.id -UserId $userID -Email $email.IsPresent -Phone $Phone.IsPresent -Ticket $ticket.IsPresent
+
+                                if ($nnotifications) {
+                                    foreach ($nnotification in $nnotifications) {
+                                        $nnotification = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $nnotification.aId | Where-Object {$_.Nid -eq $nnotification.nId}
+                                        [PSCustomObject]@{
+                                            Customer     = $customer.name
+                                            Network      = $container.name
+                                            Server       = $sensorhub.name
+                                            Agent        = $agent.name
+                                            Notification = if ($nnotification.isGroup -eq $true) {
+                                                $nnotification.surname
+                                            }
+                                            else {
+                                                $displayName = "$($nnotification.prename) $($nnotification.surname)".Trim() 
+                                                $displayName
+                                            }
+                                            EMail        = $nnotification.email
+                                            Phone        = $nnotification.phone
+                                            Ticket       = $nnotification.ticket
+                                            Defer        = if ($deferId -ne "") {
+                                                $snn = Set-SeApiAgentNotification -AuthToken $AuthToken -AId $agent.id -NId $nnotification.nId -DeferId $deferId 
+                                                $gnn = Get-SeApiAgentNotificationList -AuthToken $AuthToken -AId $Snn.aId | Where-Object {$_.Nid -eq $nnotification.nId}
+                                                [PSCustomObject]@{
+                                                    Defertime = $gnn.deferTime
+                                                    Defername = $gnn.deferName
+                                                }
+                                            }
+                                            else {
+                                                "No Deferid was set."
+                                            }
+                                            State        = "New"
+                                        }           
+                                    }
+                                }                         
+                            }                                                         
+                        }                 
+                    }
+                }
+            }
+        }
+    }
+}
