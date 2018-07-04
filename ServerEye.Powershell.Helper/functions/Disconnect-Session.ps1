452250f89b4490ef0aca3f75337863b4a2f10dc9
georg2902 <georg2902@gmx.de>
2018-07-04 12:17:40 +0200

    Update Disconnect-Session.ps1
    - added parameter to the Invoke-WebRequest Command to avoid problems when using IE especially when its not even installed

--- ServerEye.Powershell.Helper/functions/Disconnect-Session.ps1
+++ ServerEye.Powershell.Helper/functions/Disconnect-Session.ps1
@@ -11,8 +11,9 @@
 
 function Disconnect-Session ($Session) {
     $Session = Test-Auth -AuthToken $Sessions
-    Invoke-WebRequest -Uri https://api.server-eye.de/2/auth/logout -WebSession $Session | Out-Null
-
+    Invoke-WebRequest -Uri https://api.server-eye.de/2/auth/logout -WebSession $Session -UseBasicParsing | Out-Null
+    # adding parameter -UseBasicParsing avoids problems when using IE, because it provides a work-around for the first-launch configuration
+    
     Remove-Variable -Name ServerEyeGlobalSession -Scope Global -ErrorAction SilentlyContinue | Out-Null
 
     Write-Output "The Server-Eye session has been closed."
