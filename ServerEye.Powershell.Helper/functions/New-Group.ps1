a6feccd38131f2462ef5917ae4edd5bb71c4545d
Rene Thulke <rene.thulke@server-eye.de>
2018-10-12 10:51:25 +0200

    Added function New-SEGroup

---
+++ ServerEye.Powershell.Helper/functions/New-Group.ps1
@@ -0,0 +1,42 @@
+ <#
+    .SYNOPSIS
+    Creates a new Group in the OCC.
+    
+    .DESCRIPTION
+    Creates a new Group in the OCC.
+    
+    .PARAMETER Name
+    The Name for the New Group.
+
+    .PARAMETER customerId
+    The ID of the Customer the Group should .
+    
+    .PARAMETER AuthToken
+    Either a session or an API key. If no AuthToken is provided the global Server-Eye session will be used if available.
+#>
+function New-Group {
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipelineByPropertyName,Mandatory=$true)]
+        [string]$customerId,
+        [Parameter(Mandatory=$true)]
+        [string]$Name,
+        [Parameter()]
+        [alias("ApiKey","Session")]
+        $AuthToken
+    )
+    Begin{
+        $AuthToken = Test-SEAuth -AuthToken $AuthToken
+    }
+
+    Process {
+                $Group = New-SeApiGroup -AuthToken $AuthToken -customerId $companyName -name $Name
+                [PSCustomObject]@{
+                    Name = $Group.Surname
+                    GroupId = $Group.gid
+                    CustomerID = $Group.customerId
+                }
+            }
+}
+
+
