62ac48ca2d9509e58ca641395f18fbbdb0a3f984
Rene Thulke - Server-Eye <rene.thulke@kraemer-it.de>
2018-12-13 16:52:44 +0100

    Add Script AviraUninstall

---
+++ UninstallAvira/Readme.md
@@ -0,0 +1,25 @@
+## Uninstall-Avira.ps1
+
+Removes Avira from the System.
+
+## Download
+
+Please download the helper script with following powershell command:
+```
+iwr "https://raw.githubusercontent.com/Server-Eye/helpers/master/UninstallAvira/UninstallAvira.ps1" -OutFile UninstallAvira.ps1
+```
+
+## Call
+``` 
+UninstallAvira-ps1
+```
+
+## Call with Restart
+```
+AddNotificationsByCustomerAndAgentId.ps1 -Restart
+```
+
+## Parameters
+
+### Restart
+Will Restart the System a the end.
---
+++ UninstallAvira/UninstallAvira.ps1
@@ -0,0 +1,29 @@
+﻿<# 
+    .SYNOPSIS
+    Uninstall Avira Antivirus and the Avira Launcher.
+
+    .DESCRIPTION
+    This Script will Uninstall Avira Antivirus and the Avira Launcher form the System.
+
+    .PARAMETER Restart
+    Will Restart the System after the uninstall is finished.
+    
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$false)]
+    [switch]$Restart
+)
+
+Write-Host "Performing uninstallation of Avira Antivirus"
+Start-Process -FilePath "C:\Program Files\Avira\Antivirus\presetup.exe" -ArgumentList "/remsilent"
+
+$avira = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {($_.Displayname -eq "Avira") -and ($_.QuietUninstallString -like '"C:\ProgramData\Package Cache\*\Avira.OE.Setup.Bundle.exe" /uninstall /quiet')}
+Write-Host "Performing uninstallation of Avira Launcher"
+
+Start-Process -FilePath $avira.BundleCachePath -Wait -ArgumentList "/uninstall /quiet"
+
+If ($Restart.IsPresent -eq $true){
+    Restart-Computer -Force
+}
+
