0d68643c3165f16f818552de7cad5efb4691b388
Rene Thulke - Server-Eye <rene.thulke@kraemer-it.de>
2019-03-18 13:39:35 +0100

    Added Help to Function Get-Inventory

--- ServerEye.Powershell.Helper/functions/Get-Inventory.ps1
+++ ServerEye.Powershell.Helper/functions/Get-Inventory.ps1
@@ -1,29 +1,29 @@
 <#
     .SYNOPSIS
-    Get a list of all OCC-Connector for the given customer. 
+    Get the Inventory for a System.
 
-    .PARAMETER Filter
-    Filter the list to show only matching OCC-Connector. OCC-Connector are filterd based on the name of the OCC-Connector.
+    .DESCRIPTION
+    Get the Inventory for a System, shows CPU, RAM, HDD Name, HDD Capacity, HDD Free Space, OS and OS Procuktkey.
 
-    .PARAMETER CustomerId
-    The customer id for which the OCC-Connector will be displayed.
+    .PARAMETER SensorhubId
+    The sensorhub id for which the Inventory will be displayed.
 
-    .PARAMETER ConnectorID
-    The OCC-Connector with this ID will be displayed.
-    
     .PARAMETER AuthToken
     Either a session or an API key. If no AuthToken is provided the global Server-Eye session will be used if available.
 
     .EXAMPLE 
-    Get-SECustomer -Filter "Systemmanager*" | Get-SEOCCConnector
+    Get-SECustomer -Filter "*Support" | Get-SESensorhub | Get-SEInventory
 
-    Customer         Name                 ConnectorID                          MachineName
-    --------         ----                 -----------                          -----------
-    Systemmanager IT kraemerit.de         e7ef5c26-7f76-4a51-94a7-0e5046a85d55 NB-RP-T460S
-    Systemmanager IT kraemerit.de         e408d8fa-a4e1-46d5-9a11-a449e130e6d2 NB-STS
-    Systemmanager IT lab.server-eye.local a3738817-4b84-4418-8629-ce2c11f13678 DC
-    Systemmanager IT Roadshow AiO 2017    b933bac0-954c-4b4e-958b-06d05d20e8ea AIO-1248...
-    Systemmanager IT WORKGROUP            918c8ec3-d879-46c2-81a6-6b5ed9156899 NUC-1309...
+    Customer      : Server-Eye Support
+    OCC Connector : kraemerit.de
+    Sensorhub     : NB-RT-NEW
+    CPU           : Core i5-7300U
+    RAM           : 8117
+    HDD Name      : C
+    HDD Kap.      : 241958
+    HDD Free      : 158953
+    OS            : Windows 10
+    OS Procuktkey : *****************************
 
 
     .LINK 
@@ -64,7 +64,7 @@ function formatInvetoryBySensorhub ($SensorhubId, $auth) {
         CPU             = $inventory.cpu.CPUName
         RAM             = $inventory.Memory.PHYSICALTOTAL 
         "HDD Name"      = $(if ($inventory.disk.FILESYSTEM -eq "NTFS") {$inventory.disk.Disk})
-        "HDD Kap."      = $(if (($inventory.disk.FILESYSTEM) -eq "NTFS") {$inventory.disk.Capacity}) 
+        "HDD Capacity."      = $(if (($inventory.disk.FILESYSTEM) -eq "NTFS") {$inventory.disk.Capacity}) 
         "HDD Free"      = $(if (($inventory.disk.FILESYSTEM) -eq "NTFS") {$inventory.disk.Freespace})
         OS              = $inventory.Os.OSname
         "OS Procuktkey" = $inventory.OS.PRODUCTKEY
