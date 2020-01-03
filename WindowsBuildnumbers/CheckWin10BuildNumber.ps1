43adec10fa41272acab6af305888579f4318ba16
Rene Thulke - Server-Eye <rene.thulke@kraemer-it.de>
2020-01-03 14:01:56 +0100

    Added Connection Check.

--- WindowsBuildnumbers/CheckWin10BuildNumber.ps1
+++ WindowsBuildnumbers/CheckWin10BuildNumber.ps1
@@ -1,3 +1,32 @@
+# Check if module ServerEye Helper is installed, if not install it
+if (!(Get-Module -ListAvailable -Name "ServerEye.Powershell.Helper")) {
+    Write-Host "ServerEye PowerShell Module is not installed. Installing it..." -ForegroundColor Red
+    Install-Module "ServerEye.Powershell.Helper" -Scope CurrentUser -Force
+}
+
+# Check if module is loaded, if not load it
+if (!(Get-Module "ServerEye.Powershell.Helper")) {
+    Import-Module ServerEye.Powershell.Helper
+}
+
+try {
+    # Check for existing session
+    $AuthToken = Test-SEAuth -AuthToken $AuthToken
+}
+catch {
+    # There is no session - prompt for login
+    $AuthToken = Connect-SESession -Persist
+}
+
+if (!$AuthToken) {
+    $AuthToken = Connect-SESession -Persist
+}
+
+if (!$AuthToken) {
+    Write-Error "Fehler beim Login!"
+    exit 1
+}
+
 #Get the Release info from Github and shorten the Buildnumber to a usefull format
 #Example from 18363.535 to 18363
 $releaseCSV = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Server-Eye/helpers/master/WindowsBuildnumbers/Windows10Release.csv' | ConvertFrom-Csv -Delimiter "," | ForEach-Object -Process {$_."OS Build" = ($_."OS Build").Remove(5);$_}
