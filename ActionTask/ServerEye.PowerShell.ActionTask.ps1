6641bfa9945b3d14657e5cee4c1b1f14705695ae
Rene Thulke - Server-Eye <rene.thulke@kraemer-it.de>
2020-02-25 13:55:35 +0100

    Changes Modul Check to  #Requires

--- ActionTask/ServerEye.PowerShell.ActionTask.ps1
+++ ActionTask/ServerEye.PowerShell.ActionTask.ps1
@@ -1,4 +1,6 @@
-﻿ <# 
+﻿ #Requires -Version 5.0
+ #Requires -Modules ServerEye.PowerShell.Helper
+ <# 
     .SYNOPSIS
     Executes an user defined action based on the condition of a given agent.
 
@@ -13,7 +15,7 @@
     This neccessary agent ID, representing the agent you want to be monitored.You can find the ID in the settings panel of an agent (OCC)
 #>
 
-#Requires -Version 5.0
+
  
 Param(
     [Parameter(Mandatory=$true)] 
@@ -24,35 +26,7 @@ Param(
  )
 
 Begin{
-         
-    $exitCode = -1
-    $requirementsFullfilled = $false
-
-    if( Get-Module -ListAvailable -Name ServerEye.PowerShell.Helper) {
-     
-        if( !(Get-Module -Name ServerEye.PowerShell.Helper) ){
-            Import-Module -Name ServerEye.Powershell.Helper
-        }
-    
-        Write-Host "Helpers found..Continue with script"
-        $requirementsFullfilled=$true
-    
-    }else{
-    
-        try{
-    
-            Install-Module -Name ServerEye.Powershell.Helper -Scope CurrentUser -SkipPublisherCheck $true
-            Write-Host "Helpers not found..installed module!"
-            $requirementsFullfilled=$true
-    
-        }catch{
-            $exitCode = -3
-            Write-Error -Message "Helpers not found and could not be installed. Please refer to https://github.com/Server-Eye/helpers/tree/master/ServerEye.Powershell.Helper for more information" -ErrorId 3
-            exit $exitCode
-        }
-    }
-
-    $AuthToken = Test-SEAuth -AuthToken $AuthToken
+     $AuthToken = Test-SEAuth -AuthToken $AuthToken
 }
 
 Process {
