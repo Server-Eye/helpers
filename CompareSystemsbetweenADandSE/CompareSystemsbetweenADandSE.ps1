61258c7066248b2e37b90f80e0597a541bed2bc0
Rene Thulke - Server-Eye <rene.thulke@kraemer-it.de>
2020-03-13 15:41:57 +0100

    Changed to new requirements and Login Checkk

--- CompareSystemsbetweenADandSE/CompareSystemsbetweenADandSE.ps1
+++ CompareSystemsbetweenADandSE/CompareSystemsbetweenADandSE.ps1
@@ -1,3 +1,6 @@
+#Requires -Module ServerEye.PowerShell.Helper
+#Requires -Module ImportExcel
+
 [CmdletBinding(DefaultParameterSetName="ADCheck")]
 Param(
     [Parameter(ValueFromPipeline = $true)]
@@ -11,37 +14,16 @@ Param(
     [Switch]$SECheck
 )
 
-# Check if module is installed, if not install it
-if (!(Get-Module -ListAvailable -Name "ServerEye.Powershell.Helper")) {
-    Write-Host "ServerEye PowerShell Module is not installed. Installing it..." -ForegroundColor Red
-    Install-Module "ServerEye.Powershell.Helper" -Scope CurrentUser -Force
+if ($authoken -eq [String]) {
+    Connect-SESession -apikey $AuthToken    
 }
+$AuthToken = Test-SEAuth -AuthToken $AuthToken
 
-# Check if module is loaded, if not load it
-if (!(Get-Module "ServerEye.Powershell.Helper")) {
-    Import-Module ServerEye.Powershell.Helper
-}
 
-try {
-    # Check for existing session
-    $AuthToken = Test-SEAuth -AuthToken $AuthToken
-}
-catch {
-    # There is no session - prompt for login
-    $AuthToken = Connect-SESession
-}
-
-if (!$AuthToken) {
-    $AuthToken = Connect-SESession
-}
-
-if (!$AuthToken) {
-    Write-Error "Fehler beim Login!"
-}
 
 $diff = Get-ADComputer -Filter * -Property *
 
-$ref = Get-SECustomer -CustomerId $CustomerID| Get-SESensorhub
+$ref = Get-SECustomer -CustomerId $CustomerID | Get-SESensorhub
 
 $comp = Compare-Object -ReferenceObject $ref -DifferenceObject $diff -Property Name #| Select-Object -Property @{Name=("Systems with ServerEye but not in the AD:"); Expression={$_.Inputobject}}
 
