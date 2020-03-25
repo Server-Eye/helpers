43f0816de36a6a026ed7186ed29f6f67aabab418
Rene Thulke - Server-Eye <rene.thulke@kraemer-it.de>
2020-03-25 17:07:47 +0100

    Added Output for Hashvalue

--- UsefullScripts/CheckHashOfWsuscab/CheckHashOfWSUSCab.ps1
+++ UsefullScripts/CheckHashOfWsuscab/CheckHashOfWSUSCab.ps1
@@ -1,7 +1,7 @@
 $SE_occServer = "occ.server-eye.de"
 $SE_baseDownloadUrl = "https://$SE_occServer/download"
 $SE_cloudIdentifier = "se"
-$wc= new-object system.net.webclient
+$wc = new-object system.net.webclient
 $SE_Version = $wc.DownloadString("$SE_baseDownloadUrl/$SE_cloudIdentifier/currentVersion")
 $SE_Path = "C:\Program Files (x86)\Server-Eye\service"
 $CABPath = "brct\Wsusscn2.cab"
@@ -14,18 +14,26 @@ if (Test-Path -Path "$SE_Path\$SE_Version\$CABPath" ) {
     $MD5 = get-filehash "$SE_Path\$SE_Version\$CABPath" -Algorithm MD5
     if ($cabsha1.hash -notlike ($sha1).ToUpper -or $cabMD5.hash -notlike ($MD5).ToUpper) {
         Write-Output "The Hashes og the WSUS Cab are wrong, maybe the File is older or newer"
-    }else {
+        Write-Output "Current SHA1: $($cabsha1.hash) "
+        Write-Output "Current MD5: $($cabMD5.hash)"
+    }
+    else {
         Write-Output "It is the right wsusscn2.cab"
     }    
-}elseif (Test-Path -Path "$SE_Path\1\$CABPath") {
+}
+elseif (Test-Path -Path "$SE_Path\1\$CABPath") {
     $SHA1 = get-filehash "$SE_Path\1\$CABPath"-Algorithm SHA1
     $MD5 = get-filehash "$SE_Path\1\$CABPath" -Algorithm MD5
     if ($cabsha1.hash -notlike ($sha1).ToUpper -or $cabMD5.hash -notlike ($MD5).ToUpper) {
         Write-Output "The Hashes og the WSUS Cab are wrong, maybe the File is older or newer"
-    }else {
+        Write-Output "Current SHA1: $($cabsha1.hash) "
+        Write-Output "Current MD5: $($cabMD5.hash)"
+    }
+    else {
         Write-Output "It is the right wsusscn2.cab"
     }  
-}else{
+}
+else {
     Write-Output "Not Cab File found please check manually"
 }
 
