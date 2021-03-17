10645378f4f77478a453946f9fe6dbdff3796e5b
Rene Thulke <rene.thulke@server-eye.de>
2021-03-17 14:29:55 +0100

    Added Script deleted Sensorhubs

---
+++ UsefullScripts/GetDeletedSensorhubs.ps1
@@ -0,0 +1,80 @@
+<# 
+    .SYNOPSIS
+    Shows all deleted Containers.
+
+    .DESCRIPTION
+    Shows all deleted Containers for each customer within the given timeframe.
+
+    .PARAMETER customerID 
+    ID of the Customer that should be checked, if no given all Customers will be checked.
+
+    .PARAMETER TimeToAdd 
+    Time that should be added to the Date or subtracted. Example -1 would be back in time.
+
+    .PARAMETER TimeFrame 
+    Length of the frame set with TimetoAdd, Years, Months Days etc.
+
+    .PARAMETER Apikey 
+    The api-Key of the user. ATTENTION only nessesary if no Server-Eye Session exists in den Powershell
+    
+#>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $false)]
+    [string]
+    $customerID,
+    [Parameter(Mandatory = $true)]
+    [int]
+    $TimeToAdd,
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("AddYears", "AddMonths", "AddDays", "AddHours", "AddMinutes", "AddSeconds", "AddMilliseconds")]
+    $TimeFrame,
+    [Parameter(ValueFromPipeline = $true)]
+    [alias("ApiKey", "Session")]
+    $AuthToken
+
+)
+
+#region internal variables
+$Type = 103
+$start = (Get-Date).($TimeFrame)($TimeToAdd)
+$end = Get-Date
+$startMS = (([DateTimeOffset](($start).ToUniversalTime())).ToUnixTimeMilliseconds())
+$endMS = (([DateTimeOffset](($end).ToUniversalTime())).ToUnixTimeMilliseconds())
+#endregion internal variables
+
+#region internal function
+#endregion internal function
+
+
+$authtoken = Test-SEAuth -authtoken $authtoken
+
+$timespan = New-TimeSpan -Start $start -End $end
+Write-Output "Deleted Sensorhubs in the last $($timespan.Days) Days"
+if ($customerID) {
+    $customers = Get-SeApiCustomerList -authtoken $authtoken | Where-Object {$_.cid -eq $customerID}
+}else {
+    $customers = Get-SeApiCustomerList -authtoken $authtoken
+}
+
+foreach ($customer in $customers) {
+    try {
+        $Containers = Get-SeApiActionlogList -AuthToken $authtoken -Type $Type -Start $startms -End $endMS -Of $customer.cid
+        if ($Containers) {
+            foreach ($Container in $Containers) {
+                [PSCustomObject]@{
+                    Kunde         = $customer.Companyname
+                    ContainerID   = if($Container.target.cid){$Container.target.cid}else {$Container.target.id} 
+                    ContainerName = $Container.target.name
+                } 
+            } 
+        }
+    }
+    catch {
+        Write-Error $_
+    }
+}
+
+
+
