2641e542fc1d0d29a9d56bf6a62119158ae0c82f
Rene Thulke <rene.thulke@server-eye.de>
2021-04-06 12:57:04 +0200

    Changed to 1 API call

--- ServersAndWorkstationsWithSensors/ServersAndWorkstationsWithSensors.ps1
+++ ServersAndWorkstationsWithSensors/ServersAndWorkstationsWithSensors.ps1
@@ -1,7 +1,7 @@
 ﻿[CmdletBinding()]
 Param(
-    [Parameter(ValueFromPipeline=$true)]
-    [alias("ApiKey","Session")]
+    [Parameter(ValueFromPipeline = $true)]
+    [alias("ApiKey", "Session")]
     $AuthToken,
     [switch]$Workstation,
     [switch]$Server
@@ -9,73 +9,62 @@ Param(
 
 $AuthToken = Test-SEAuth -AuthToken $AuthToken
 
-if (($Server-eq $false) -and ($Workstation -eq $false) ){
-$both = $true
-}
+$data = Get-SeApiMyNodesList -Filter customer, agent, container -AuthToken $AuthToken
+
 $result = @()
-
-$customers = Get-SeApiMyNodesList -Filter customer -AuthToken $AuthToken
+$customers = $Data | Where-Object { $_.Type -eq 1 }
+$Connectors = $Data | Where-Object {$_.Type -eq 2 -and $_.subtype -eq 0}
 foreach ($customer in $customers) {
-    $containers = Get-SeApiCustomerContainerList -AuthToken $AuthToken -CId $customer.id
-
+    $Sensorhubs = $Data | Where-Object { $_.Type -eq 2 -and $_.subtype -eq 2 -and $_.customerId -eq $customer.id }
     
-    foreach ($container in $containers) {
+    foreach ($Sensorhub in $Sensorhubs) {
+        $Connector = $Connectors | Where-Object {$_.id -eq $Sensorhub.parentId}
+        If (($Server -eq $false) -and ($Workstation -eq $false)) {
 
-        if ($container.subtype -eq "0") {
+            $Agents = $Data | Where-Object { $_.Type -eq 3 -and $_.parentId -eq $Sensorhub.id}
 
-            foreach ($sensorhub in $containers) {
-                
-              if ($sensorhub.subtype -eq "2" -And $sensorhub.parentId -eq $container.id) {
-
-                If ($both -eq $true){
-
-                     $agents = Get-SeApiContainerAgentList -AuthToken $AuthToken -CId $sensorhub.id
-
-                         foreach ($agent in $agents) {
-                            $out = New-Object psobject
-                            $out | Add-Member NoteProperty Kunde ($customer.name)
-                            $out | Add-Member NoteProperty Netzwerk ($container.name)
-                            $out | Add-Member NoteProperty Server ($sensorhub.name)
-                            $out | Add-Member NoteProperty Sensor ($agent.name)
-                            $out | Add-Member NoteProperty Tag ($agent.tags.name)                                                     
-                            $result += $out
-                            }
-                }
-               If ($Workstation -eq $true){
-                    if ($sensorhub.isServer -eq $false) {
-
-                        $agents = Get-SeApiContainerAgentList -AuthToken $AuthToken -CId $sensorhub.id
-
-                            foreach ($agent in $agents) {
-                            $out = New-Object psobject
-                            $out | Add-Member NoteProperty Kunde ($customer.name)
-                            $out | Add-Member NoteProperty Netzwerk ($container.name)
-                            $out | Add-Member NoteProperty Server ($sensorhub.name)
-                            $out | Add-Member NoteProperty Sensor ($agent.name)
-                            $out | Add-Member NoteProperty Tag ($agent.tags.name)                                                     
-                            $result += $out
-                            }
-                        }
-                }
-                If ($Server -eq $true){
-                    if ($sensorhub.isServer -eq $true) {
-
-                        $agents = Get-SeApiContainerAgentList -AuthToken $AuthToken -CId $sensorhub.id
-
-                            foreach ($agent in $agents) {
-                            $out = New-Object psobject
-                            $out | Add-Member NoteProperty Kunde ($customer.name)
-                            $out | Add-Member NoteProperty Netzwerk ($container.name)
-                            $out | Add-Member NoteProperty Server ($sensorhub.name)
-                            $out | Add-Member NoteProperty Sensor ($agent.name)
-                            $out | Add-Member NoteProperty Tag ($agent.tags.name)                                                     
-                            $result += $out
-                            }
-                     }
-                }
+            foreach ($agent in $agents) {
+                $out = New-Object psobject
+                $out | Add-Member NoteProperty Kunde ($customer.name)
+                $out | Add-Member NoteProperty Netzwerk ($Connector.name)
+                $out | Add-Member NoteProperty Server ($sensorhub.name)
+                $out | Add-Member NoteProperty Sensor ($agent.name)
+                $out | Add-Member NoteProperty Tag ($agent.tags.name)                                                     
+                $result += $out
+            }
         }
     }
-}
-}
+    If ($Workstation -eq $true) {
+        if ($sensorhub.isServer -eq $false) {
+
+            $Agents = $Data | Where-Object { $_.Type -eq 3 -and $_.parentId -eq $Sensorhubs.id }
+
+            foreach ($agent in $agents) {
+                $out = New-Object psobject
+                $out | Add-Member NoteProperty Kunde ($customer.name)
+                $out | Add-Member NoteProperty Netzwerk ($Connector.name)
+                $out | Add-Member NoteProperty Server ($sensorhub.name)
+                $out | Add-Member NoteProperty Sensor ($agent.name)
+                $out | Add-Member NoteProperty Tag ($agent.tags.name)                                                     
+                $result += $out
+            }
+        }
+    }
+    If ($Server -eq $true) {
+        if ($sensorhub.isServer -eq $true) {
+
+            $Agents = $Data | Where-Object { $_.Type -eq 3 -and $_.parentId -eq $Sensorhubs.id }
+
+            foreach ($agent in $agents) {
+                $out = New-Object psobject
+                $out | Add-Member NoteProperty Kunde ($customer.name)
+                $out | Add-Member NoteProperty Netzwerk ($Connector.name)
+                $out | Add-Member NoteProperty Server ($sensorhub.name)
+                $out | Add-Member NoteProperty Sensor ($agent.name)
+                $out | Add-Member NoteProperty Tag ($agent.tags.name)                                                     
+                $result += $out
+            }
+        }
+    }
 }
 $result
