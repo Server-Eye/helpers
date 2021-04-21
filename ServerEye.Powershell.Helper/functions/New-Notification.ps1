5c6d46fe05f0ac5f32aa141f557c8003380e8651
Rene Thulke <rene.thulke@server-eye.de>
2021-04-21 13:24:17 +0200

    Another Bug fix

--- ServerEye.Powershell.Helper/functions/New-Notification.ps1
+++ ServerEye.Powershell.Helper/functions/New-Notification.ps1
@@ -80,7 +80,7 @@ function NewNotificationofSensor {
     $Authtoken
     )
     $noti = New-SeApiAgentNotification -AuthToken $Authtoken -AId $sensorId -UserId $UserId -Email $SendEmail -Phone $SendTextmessage -Ticket $SendTicket
-    formatSensorNotificationNew -Authtoken $Authtoken -notiID $noti.nid -sensorid $noti.id  -DeferId $deferid
+    formatSensorNotificationNew -Authtoken $Authtoken -notiID $noti.nid -sensorid $noti.parent_id  -DeferId $deferid
 }
 
 function formatSensorNotificationNew($notiID, $Authtoken, $SensorId, $deferid){
@@ -149,7 +149,7 @@ function NewNotificationofContainer {
         $customerName = $sensorhub.Customer
     }
     
-    formatContainerNotificationNew -notiID $noti.nid -authoken $Authtoken -SensorhubID $noti.id -deferid $deferId
+    formatContainerNotificationNew -notiID $noti.nid -authoken $Authtoken -SensorhubID $noti.parent_id -deferid $deferId
 
 }
 
