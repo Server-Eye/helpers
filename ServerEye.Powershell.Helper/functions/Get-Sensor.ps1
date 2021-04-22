eda9f90b7af5b433f70e0b4259a0beca9bfbf821
Rene Thulke <rene.thulke@server-eye.de>
2021-04-22 11:25:24 +0200

    Added SensorType as Call

--- ServerEye.Powershell.Helper/functions/Get-Sensor.ps1
+++ ServerEye.Powershell.Helper/functions/Get-Sensor.ps1
@@ -1,4 +1,4 @@
- <#
+<#
     .SYNOPSIS
     Get sensor details. 
     
@@ -18,23 +18,27 @@
     Either a session or an API key. If no AuthToken is provided the global Server-Eye session will be used if available.
 #>
 function Get-Sensor {
-    [CmdletBinding(DefaultParameterSetName="bySensorhub")]
+    [CmdletBinding(DefaultParameterSetName = "bySensorhub")]
     Param(
-        [Parameter(Mandatory=$false,ParameterSetName="bySensorhub",Position=0)]
+        [Parameter(Mandatory = $false, ParameterSetName = "bySensorhub", Position = 0)]
         [string]$Filter,
-        [parameter(ValueFromPipelineByPropertyName,ParameterSetName="bySensorhub")]
+        [parameter(ValueFromPipelineByPropertyName, ParameterSetName = "bySensorhub")]
         $SensorhubId,
-        [parameter(ValueFromPipelineByPropertyName,ParameterSetName="bySensor")]
+        [parameter(ValueFromPipelineByPropertyName, ParameterSetName = "bySensor")]
         $SensorId,
-        [parameter(Mandatory=$false,ValueFromPipelineByPropertyName,ParameterSetName="bySensor")]
-        [parameter(Mandatory=$false,ValueFromPipelineByPropertyName,ParameterSetName="bySensorhub")]
+        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName, ParameterSetName = "bySensorType")]
+        $SensorType,
+        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName, ParameterSetName = "bySensor")]
+        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName, ParameterSetName = "bySensorhub")]
+        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName, ParameterSetName = "bySensorType")]
         [switch]$ShowFree,
-        [Parameter(Mandatory=$false,ParameterSetName="bySensor")]
-        [Parameter(Mandatory=$false,ParameterSetName="bySensorhub")]
+        [Parameter(Mandatory = $false, ParameterSetName = "bySensor")]
+        [Parameter(Mandatory = $false, ParameterSetName = "bySensorhub")]
+        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName, ParameterSetName = "bySensorType")]
         $AuthToken
     )
 
-    Begin{
+    Begin {
         $AuthToken = Test-SEAuth -AuthToken $AuthToken
         cacheSensorTypes -auth $AuthToken
         $agentList = Get-SeApiMyNodesList -Filter agent -AuthToken $AuthToken
@@ -44,10 +48,19 @@ function Get-Sensor {
         if ($SensorhubId) {
             Write-Debug "SensorhubID will be used $SensorhubId"
             getSensorBySensorhub -sensorhubId $SensorhubId -filter $Filter -auth $AuthToken
-        } elseif ($SensorId) {
+        }
+        elseif ($SensorId) {
             Write-Debug "SensorID will be used $SensorId"
             getSensorById -sensorId $SensorId -auth $AuthToken
-        } else {
+        }
+        elseif ($Sensortype) {
+            Write-Debug "SensorType will be used $Sensortype"
+            $agents = $agentList | Where-Object { $_.agentType -eq $SensorType }
+            foreach ($agent in $agents) {
+                formatSensorByType -auth $authtoken -agent $agent
+            }
+        } 
+        else {
             Write-Error "Please provide a SensorhubId or a SensorId."
         }
     }
@@ -102,22 +115,42 @@ function getSensorById ($sensorId, $auth) {
     $sensorhub = Get-SESensorhub -SensorhubId $sensor.parentId -AuthToken $auth 
     formatSensor -sensor $sensor -auth $auth -sensorhub $sensorhub -agentList $agentList
 }
-function formatSensor($sensor, $sensorhub,$agentlist, $auth) {
+function formatSensor($sensor, $sensorhub, $agentlist, $auth) {
     $type = $Global:SensorTypes.Get_Item($sensor.type)
-    $state = Get-SeApiAgentStateListbulk -AId $sensor.aId -AuthToken $authtoken
-    $notification = $agentList | Where-Object {$_.id -eq $sensor.aId}
+    $notification = $agentList | Where-Object { $_.id -eq $sensor.aId }
     $SESensor = [PSCustomObject]@{
-        Name = $sensor.name
-        SensorType = $type.defaultName
-        SensorTypeID = $type.agentType
-        SensorId = $sensor.aId
-        Interval = $sensor.interval
-        Error = $sensor.state -or $sensor.forceFailed
-        HasNotification = If(!$notification){"Your are not a Manager of this customer, so we can't show that."}else{$notification.hasNotification}
-        Sensorhub = $sensorhub.name
+        Name            = $sensor.name
+        SensorType      = $type.defaultName
+        SensorTypeID    = $type.agentType
+        SensorId        = $sensor.aId
+        Interval        = $sensor.interval
+        Error           = $notification.state
+        HasNotification = If (!$notification) { "Your are not a Manager of this customer, so we can't show that." }else { $notification.hasNotification }
+        Sensorhub       = $sensorhub.name
         'OCC-Connector' = $sensorhub.'OCC-Connector'
-        Customer = $sensorhub.customer
-        Message = $state.message
+        Customer        = $sensorhub.customer
+        Message         = $state.message
+    }
+    if ($ShowFree) {
+        Add-Member -MemberType NoteProperty -Name forFree -Value $type.forFree -InputObject $SESensor
+    }
+    Return $SESensor
+}
+
+function formatSensorByType($auth, $agent) {
+    $type = $Global:SensorTypes.Get_Item($agent.agentType)
+    $sensorhub = Get-SESensorhub -SensorhubId $agent.parentId -AuthToken $auth
+    $SESensor = [PSCustomObject]@{
+        Name            = $agent.name
+        SensorType      = $type.defaultName
+        SensorTypeID    = $type.agentType
+        SensorId        = $agent.Id
+        Error           = $agent.state
+        HasNotification = $agent.hasNotification
+        Sensorhub       = $sensorhub.name
+        'OCC-Connector' = $sensorhub.'OCC-Connector'
+        Customer        = $sensorhub.customer
+        Message         = $agent.message
     }
     if ($ShowFree) {
         Add-Member -MemberType NoteProperty -Name forFree -Value $type.forFree -InputObject $SESensor
