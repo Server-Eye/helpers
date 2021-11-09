6f702fbe12924749b4f18a0ed1268a6743c36f2c
Rene Thulke <rene.thulke@server-eye.de>
2021-11-09 09:19:39 +0100

    Added auto Check for SE Path

--- UsefullScripts/WindowsUpdate/SmartUpdates/offline-scan.ps1
+++ UsefullScripts/WindowsUpdate/SmartUpdates/offline-scan.ps1
@@ -1,9 +1,13 @@
 #Using WUA to Scan for Updates Offline with PowerShell  
 #VBS version: https://docs.microsoft.com/en-us/previous-versions/windows/desktop/aa387290(v=vs.85)  
   
+$Servicepath = Get-CimInstance -ClassName win32_service -Filter "Name like 'CCService'" | Select-Object PathName
+$Path = $Servicepath.PathName -replace '"','' | Split-Path
+$cab = Join-Path -Path $Path -ChildPath "brct\wsusscn2.cab"
+
 $UpdateSession = New-Object -ComObject Microsoft.Update.Session  
 $UpdateServiceManager  = New-Object -ComObject Microsoft.Update.ServiceManager  
-$UpdateService = $UpdateServiceManager.AddScanPackageService("Offline Sync Service", "C:\Program Files (x86)\Server-Eye\service\993\brct\wsusscn2.cab", 1)  
+$UpdateService = $UpdateServiceManager.AddScanPackageService("Offline Sync Service", "$cab", 1)  
 $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()   
   
 Write-Output "Searching for updates... `r`n"  
@@ -25,8 +29,12 @@ if($Updates.Count -eq 0){
   
 Write-Output "List of applicable items on the machine when using wssuscan.cab: `r`n"  
   
-$i = 0  
+$i = 1
 foreach($Update in $Updates){   
-    Write-Output "$($i)> $($Update.Title)"  
-    $i++  
+    [PSCustomObject]@{
+        Number = $i
+        Update = $Update.Title
+    }
+    $i ++
 }
+
