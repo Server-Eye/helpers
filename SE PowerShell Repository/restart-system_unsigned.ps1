504e152406d9f37d6ddccde156b7b1192166c3f9
StefanieSander <stefanie.sander@server-eye.de>
2022-07-13 11:59:56 +0200

    Changed Outputs to Write-Host and Write-Error

--- SE PowerShell Repository/restart-system_unsigned.ps1
+++ SE PowerShell Repository/restart-system_unsigned.ps1
@@ -4,33 +4,33 @@
  
     .DESCRIPTION
         Execution of this script forces an system restart after 10 seconds.
-
+ 
     .PARAMETER Comment 
         Comment for the restart, default is "Neustart über die Aufgabenplanung".
-
+ 
     .PARAMETER reason 
         Reason for the system restart, can either be "P" or "U", default "P".
-
+ 
     .PARAMETER major 
         Specifies the major reason number (a positive integer, less than 256), default is 0.
-
+ 
     .PARAMETER minor 
         Specifies the minor reason number (a positive integer, less than 65536), default is 0.
-
+ 
     .PARAMETER Time 
         Time before the restart should occur, default is 10 seconds.
         
     .NOTES
         Author  : Server-Eye
-        Version : 1.1
-
+        Version : 1.3
+ 
     .Link
         https://docs.microsoft.com/de-de/windows-server/administration/windows-commands/shutdown
-
+ 
         Reasons, major and minor settings:
         https://servereye.freshdesk.com/a/solutions/articles/14000128892
 #>
-
+ 
 [CmdletBinding()]
 Param(
     [parameter(Mandatory = $false, HelpMessage = "Comment for the restart ")]
@@ -63,19 +63,34 @@ Begin {
         FilePath     = $FileToRunpath
         ArgumentList = $argument       
         NoNewWindow  = $true
+        PassThru = $true
     }
     # 0 = everything is ok
 }
-
-
  
 Process {
     try {
-        Add-Content -Path $SELog -Value "$(Get-Date -Format "yy.MM.dd hh:mm:ss") INFO  ServerEye.Task.Logic.PowerShell - Trigger system restart with Arguments: $($startProcessParams.ArgumentList)" 
-        Start-Process @startProcessParams
+        $gpos = Get-ChildItem -Path "Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\Scripts\Shutdown"
  
-    }
-    catch {
+        foreach($gpo in $gpos){
+            $gpoLocation = "Registry::\" + $gpo.Name
+            $subGpos = Get-ChildItem -Path $gpoLocation
+        
+            foreach($subGpo in $subGpos){
+                    $gpoScript = ""
+                    [string]$gpoScript = $subGpo.GetValue("Script")
+
+                    if($gpoScript.Contains("triggerPatchRun.cmd")){
+						Add-Content -Path $SELog -Value "$(Get-Date -Format "yy.MM.dd hh:mm:ss") INFO  ServerEye.Task.Logic.PowerShell - Write "force" parameter to gpo registry"
+						Set-Itemproperty -path $subGpo.PSPath -Name 'Parameters' -value 'force'
+                    }
+            }
+        }
+		
+        Add-Content -Path $SELog -Value "$(Get-Date -Format "yy.MM.dd hh:mm:ss") INFO  ServerEye.Task.Logic.PowerShell - Trigger system shutdown with Arguments: $($startProcessParams.ArgumentList)" 
+		$RestartProcess = Start-Process @startProcessParams
+		
+    } catch {
         Add-Content -Path $SELog -Value "$(Get-Date -Format "yy.MM.dd hh:mm:ss") ERROR  ServerEye.Task.Logic.PowerShell - Something went wrong: $_" # This prints the actual error
         $ExitCode = 1 
         # if something goes wrong set the exitcode to something else then 0
